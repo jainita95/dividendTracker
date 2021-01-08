183fb988f8122ecf31bfad35a81969beaacb6cf4
jainita95 <35333229+jainita95@users.noreply.github.com>
2021-01-08 00:14:24 -0800

    Update dividend_orchestrator.py

--- dividend_orchestrator.py
+++ dividend_orchestrator.py
@@ -77,7 +77,7 @@ CallDividendApi = PythonVirtualenvOperator(
 CsvLoad = PythonVirtualenvOperator(
     task_id='CsvLoad',
     python_callable=csv_load,
-    requirements=['sendgrid==6.4.8','apache-airflow','psycopg2','psycopg2-binary','libpq-dev'],
+    requirements=['sendgrid==6.4.8','apache-airflow','psycopg2-binary','libpq-dev'],
     python_version='3',
     trigger_rule='all_done',
     dag=dag
@@ -85,7 +85,7 @@ CsvLoad = PythonVirtualenvOperator(
 CalculateProbability = PythonVirtualenvOperator(
     task_id='CalculateProbability',
     python_callable=calculate_probability,
-    requirements=['sendgrid==6.4.8','apache-airflow','psycopg2','psycopg2-binary','libpq-dev'],
+    requirements=['sendgrid==6.4.8','apache-airflow','psycopg2-binary','libpq-dev'],
     python_version='3',
     trigger_rule='all_done',
     dag=dag
